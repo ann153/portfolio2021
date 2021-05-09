4efd424851ef4a5517f88b3d5f21296ee2cfc9c8
ann153 <asanada.mail+github@gmail.com>
2021-05-09 22:39:10 +0900

    add subtitle to all pages

--- template-parts/common-noslideshow.php
+++ template-parts/common-noslideshow.php
@@ -1,6 +1,6 @@
 <!-- Main Section -->
 <section class="main-section">
 	<h2><?php the_title(); ?></h2>
-	<h3><?php the_field( 'common_subtitle' ); ?></h3>
+	<h3><?php the_field( 'subtitle' ); ?></h3>
 	<p><?php the_content(); ?></p>
 </section>
